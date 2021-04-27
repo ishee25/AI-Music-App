de4bad30351083ed81a64c1f49b98d57aa7070dc
ishee25 <74249367+ishee25@users.noreply.github.com>
2021-04-27 18:38:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+song1 = "";
+song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+score_rightWrist = 0;
+score_leftWrist = 0;
+song1_status = "";
+song2_status = "";
+
+function preload(){
+    song1 = loadSound("lush_life.mp4");
+    song2 = loadSound("fireworks.mp4");
+}
+
+function setup(){
+    canvas = createCanvas(600, 500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("Posenet Is Intialized!");
+}
+
+function draw(){
+    image(video, 0, 0, 600, 500);
+
+    song1_status = song1.isPlaying();
+
+    fill("#fc036f");
+    stroke("#fc036f");
+
+    if(score_leftWrist > 0.2){
+        circle(leftWristX, leftWristY, 25);
+        song2.stop();   
+
+        if(song1_status == false){
+            song1.play();
+            document.getElementById("song_name").innerHTML = "Song Name - Lush Life";
+        }
+    }
+    
+    song2_status = song2.isPlaying();
+
+    if(score_rightWrist > 0.2){
+        circle(rightWristX, rightWristY, 25);
+        song1.stop();
+
+        if(song2_status == false){
+            song2.play();
+            document.getElementById("song_name").innerHTML = "Song Name - Fireworks";
+        }
+    }
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        score_leftWrist = results[0].pose.keypoints[9].score;
+        score_rightWrist = results[0].pose.keypoints[10].score;
+        console.log("scoreLeftWrist = " + score_leftWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.rightWrist.y;
+        console.log("Left Wrist X = " + leftWristX + "Left Wrist Y = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X = " + rightWristX + "Right Wrist Y = " + rightWristY);
+    }
+}
